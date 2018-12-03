b50cc1aa082c08a0f56b9cc444820613f9423b88
mbaradji221 <mbaradji221@outlook.fr>
2018-12-03 17:24:49 +0100

    update

--- vue/linkToDashBoardDialog.vue
+++ vue/linkToDashBoardDialog.vue
@@ -39,6 +39,7 @@
 import * as graphLib from "spinalgraph";
 const DASHBOARD_CONTEXT = "Dashboard Standard";
 const RELATION_NAME = "hasDashBoard";
+
 const ENDPOINT_RELATION_NAME = "hasEndpoint";
 
 const { SpinalEndpoint } = require("spinal-models-bmsNetwork");
