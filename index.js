8e3e0199644d4a4552fef270ea56301be770449f
mbaradji221 <mbaradji221@outlook.fr>
2019-06-14 15:33:21 +0200

    add reference context

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ import {
 import CreateContextBtn from './classes/createContext';
 import AddAbstactElement from "./classes/addAbstract";
 import AddReferenceBtn from "./classes/addReference";
+import AddToReference from "./classes/addToReferenceContext";
 
-spinalContextMenuService.registerApp(HeaderBarName, new CreateContextBtn(), [3]);
+
+spinalContextMenuService.registerApp(HeaderBarName, new CreateContextBtn(), [
+  3
+]);
 spinalContextMenuService.registerApp(sidebarName, new AddAbstactElement(), [3]);
 spinalContextMenuService.registerApp(sidebarName, new AddReferenceBtn(), [3]);
+spinalContextMenuService.registerApp(sidebarName, new AddToReference(), [3]);
