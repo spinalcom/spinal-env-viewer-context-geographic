94b5fc7899dc22398c0f837d1ac3cb26380394d7
laurent <htd.laurent@gmail.com>
2021-01-04 17:32:40 +0100

    add update dbId in bim file

--- classes/updateDbId.js
+++ classes/updateDbId.js
@@ -22,38 +22,30 @@
  * <http://resources.spinalcom.com/licenses.pdf>.
  */
 
-import ContextGeographicService from "spinal-env-viewer-context-geographic-service";
-// import {
-//   SpinalGraphService
-// } from "spinal-env-viewer-graph-service";
-
-// import bimobjService from 'spinal-env-viewer-plugin-bimobjectservice';
-// const bimobjService = window.spinal.BimObjectService;
-
-const {
-  SpinalContextApp
-} = require("spinal-env-viewer-context-menu-service");
+import { SpinalContextApp } from 'spinal-env-viewer-context-menu-service';
+import { SpinalGraphService } from 'spinal-env-viewer-graph-service';
 import { spinalPanelManagerService } from "spinal-env-viewer-panel-manager-service";
+import {
+  CONTEXT_TYPE,
+  EQUIPMENT_TYPE,
+  SITE_RELATION,
+  BUILDING_RELATION,
+  ZONE_RELATION,
+  FLOOR_RELATION,
+  ROOM_RELATION,
+  EQUIPMENT_RELATION,
+  REFERENCE_RELATION
+} from 'spinal-env-viewer-context-geographic-service/build/constants';
 
-// import {
-//   toasted
-// } from "../toats";
+const LABEL = 'Update Ids Model';
+const DESCRIPTION = "update model dbids by the externalID";
+const NODE_TYPE = 'BimFile';
 
-const constants = ContextGeographicService.constants;
-
-const geoRelations = [
-  constants.SITE_RELATION,
-  constants.BUILDING_RELATION,
-  constants.ZONE_RELATION,
-  constants.FLOOR_RELATION,
-  constants.ROOM_RELATION,
-  constants.EQUIPMENT_RELATION,
-  constants.REFERENCE_RELATION,
-  `${constants.REFERENCE_RELATION}.ROOM`
-];
-
-async function updateBimObjectId(context) {
+async function updateBimObjectId(bimObjects) {
   const mapModelExternId = new Map();
+  const updated = [];
+  const notFound = [];
+
   function getExternalIdMapping(model) {
     if (mapModelExternId.has(model)) return mapModelExternId.get(model);
     const prom = new Promise((resolve) => {
@@ -64,101 +56,107 @@ async function updateBimObjectId(context) {
     mapModelExternId.set(model, prom);
     return prom;
   }
-  const bimObjects = await context.find(geoRelations, (node) => {
-    return node.info.type.get() === constants.EQUIPMENT_TYPE;
-  });
-  const updated = [];
+
+  console.log("updateBimObjectId - number of items :", bimObjects.length);
   for (const bimObj of bimObjects) {
     const bimFileId = bimObj.info.bimFileId.get();
-    const model = spinal.BimObjectService.getModelByBimfile(bimFileId);
+    const model = window.spinal.BimObjectService.getModelByBimfile(bimFileId);
+    // eslint-disable-next-line no-await-in-loop
     const externalIdMapping = await getExternalIdMapping(model);
     const externalId = bimObj.info.externalId.get();
-    if (bimObj.info.dbid.get() !== externalIdMapping[externalId]) {
+    if (!externalIdMapping[externalId]) {
+      notFound.push(bimObj.info.get());
+      bimObj.info.dbid.set(-1);
+    } else if (bimObj.info.dbid.get() !== externalIdMapping[externalId]) {
       updated.push(externalIdMapping[externalId]);
       bimObj.info.dbid.set(externalIdMapping[externalId]);
     }
   }
-  spinalPanelManagerService.openPanel("notificationDialog",
-    { msg: "update done", time: 2000 });
   console.log("End");
+  let msg = "Update done";
+  if (updated.length > 0) {
+    console.log('UPDATED', updated);
+    msg = `${msg}, objects updated : ${updated.length}`;
+  }
+  if (notFound.length > 0) {
+    console.log('notFound', notFound);
+    msg = `${msg}, objects not updated : ${notFound.length}`;
+  }
+  spinalPanelManagerService.openPanel("notificationDialog",
+    { msg: msg, time: 3000 });
 }
 
-
-class UpdateDbId extends SpinalContextApp {
+export class UpdateBimObjectIdBtn extends SpinalContextApp {
   constructor() {
-    super("Update Ids Model", "update model dbids by the externalID", {
-      icon: "update",
-      icon_type: "in",
-      backgroundColor: "#FF0000",
-      fontColor: "#FFFFFF"
+    super(LABEL, DESCRIPTION, {
+      icon: 'update',
+      icon_type: 'in',
+      backgroundColor: '#000000',
+      fontColor: '#ffffff'
     });
   }
-
   isShown(option) {
-
-    if (option.context.type.get() === constants.CONTEXT_TYPE &&
-      option.context.id.get() === option.selectedNode.id.get()) {
+    if (option.selectedNode.type.get() === NODE_TYPE) {
       return Promise.resolve(true);
     }
-
     return Promise.resolve(-1);
-
-
   }
 
-  action(option) {
-    const node = spinal.spinalGraphService.getRealNode(option.selectedNode.id.get());
-    return updateBimObjectId(node);
-    // let elementSelected = window.spinal.ForgeViewer.viewer
-    //   .getAggregateSelection();
-
-    // if (elementSelected.length == 0) {
-    //   // toasted.error("no item selected");
-    //   return;
-    // }
-
-    // for (let idx = 0; idx < elementSelected.length; idx++) {
-    //   const {
-    //     model,
-    //     selection
-    //   } = elementSelected[idx];
-
-    //   model.getBulkProperties(
-    //     selection, {
-    //       propFilter: ["name"]
-    //     },
-    //     el => {
-    //       el.forEach(element => {
-    //         window.spinal.BimObjectService.addReferenceObject(
-    //           option.selectedNode.id.get(), element.dbId, element.name, model);
-    //       });
-    //     }
-    //   );
-    // }
-
-    // elementSelected.forEach(element => {
-
-    // let node = SpinalGraphService.getRealNode(option.selectedNode.id
-    //   .get());
-
-    // bimobjService.addReferenceObject(node, element, "bimObject_" +
-    //   element).then(el => {
-    //   if (el) {
-    //     // toasted.success("Reference added with success !");
-    //     return;
-    //   }
-    //   // toasted.error("Reference is not added !");
-    // })
-
-
-
-
-    // });
-
-
-
+  async action(option) {
+    const nodeId = option.selectedNode.id.get();
+    const rNode = SpinalGraphService.getRealNode(nodeId);
+    const bimObjects = [];
+    const bimContexts = await rNode.getChildren("hasBimContext");
+    for (const bimContext of bimContexts) {
+      // eslint-disable-next-line no-await-in-loop
+      const bimObj = await bimContext.getChildren("hasBimObject");
+      bimObjects.push(...bimObj);
+    }
+    return updateBimObjectId(bimObjects);
   }
-
 }
 
-module.exports = UpdateDbId;
+
+
+export class SpatialContextUpdateBimObjectIdBtn extends SpinalContextApp {
+  constructor() {
+    super(LABEL, DESCRIPTION, {
+      icon: 'update',
+      icon_type: 'in',
+      backgroundColor: '#000000',
+      fontColor: '#ffffff'
+    });
+  }
+  isShown(option) {
+    if (option.selectedNode.type.get() === CONTEXT_TYPE) {
+      return Promise.resolve(true);
+    }
+    return Promise.resolve(-1);
+  }
+
+  async action(option) {
+    const nodeId = option.selectedNode.id.get();
+    const rNode = SpinalGraphService.getRealNode(nodeId);
+    const bimObjects = new Set();
+
+    const geoRelations = [
+      SITE_RELATION,
+      BUILDING_RELATION,
+      ZONE_RELATION,
+      FLOOR_RELATION,
+      ROOM_RELATION,
+      EQUIPMENT_RELATION,
+      REFERENCE_RELATION,
+      "hasReferenceObject.ROOM"
+    ];
+
+    await rNode.find(geoRelations, (node) => {
+      if (typeof node.info.externalId !== "undefined" &&
+        typeof node.info.dbid !== "undefined") {
+        bimObjects.add(node);
+      }
+      return false;
+    });
+    return updateBimObjectId(bimObjects);
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2021 SpinalCom - www.spinalcom.com
+ *
+ * This file is part of SpinalCore.
+ *
+ * Please read all of the following terms and conditions
+ * of the Free Software license Agreement ("Agreement")
+ * carefully.
+ *
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ *
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 const HeaderBarName = "GraphManagerTopBar";
 const sidebarName = "GraphManagerSideBar";
 
@@ -9,7 +33,7 @@ import AddAbstactElement from "./classes/addAbstract";
 import AddReferenceBtn from "./classes/addReference";
 import AddToReference from "./classes/addToReferenceContext";
 import AddBimObjects from "./classes/addBimObject";
-import UpdateDbId from './classes/updateDbId';
+import {UpdateBimObjectIdBtn,SpatialContextUpdateBimObjectIdBtn } from './classes/updateDbId';
 
 spinalContextMenuService.registerApp(HeaderBarName, new CreateContextBtn(), [
   3
@@ -19,4 +43,5 @@ spinalContextMenuService.registerApp(sidebarName, new AddAbstactElement(), [3]);
 spinalContextMenuService.registerApp(sidebarName, new AddReferenceBtn(), [3]);
 spinalContextMenuService.registerApp(sidebarName, new AddToReference(), [3]);
 spinalContextMenuService.registerApp(sidebarName, new AddBimObjects(), [3]);
-spinalContextMenuService.registerApp(sidebarName, new UpdateDbId(), [3]);
+spinalContextMenuService.registerApp(sidebarName, new UpdateBimObjectIdBtn(), [3]);
+spinalContextMenuService.registerApp(sidebarName, new SpatialContextUpdateBimObjectIdBtn(), [3]);
